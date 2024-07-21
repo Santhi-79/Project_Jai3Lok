ef7a7c8c487cff435dbb04aeaefd2084b4b5b9ef
Sridhar <sridhar.bssv@gmail.com>
2024-07-21 21:36:44 +0530

    Adding Rating service and fedw clasees

--- Product-Service/src/main/java/com/efarms/productService/entity/Product.java
+++ Product-Service/src/main/java/com/efarms/productService/entity/Product.java
@@ -1,5 +1,208 @@
 package com.efarms.productService.entity;
 
-public class Product {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import com.efarms.RatingServices.entity.Rating;
+import com.efarms.RatingServices.entity.Review;
+
+@Entity
+@Table(name = "product")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="title")
+	private String title;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name = "price")
+	private float price;
+	
+
+    @Column(name = "discounted_price")
+    private int discountedPrice;
+    
+    @Column(name="discount_persent")
+    private int discountPersent;
+    
+    @Column(name = "quantity")
+	private int quantity;
+    
+    @Column(name = "brand")
+	private String brand;
+    
+    @Column(name="image_url")
+	private String imageUrl;
+    
+    @OneToMany(mappedBy = "product",cascade = CascadeType.ALL,orphanRemoval = true)
+	private List<Rating > ratings=new ArrayList<>();
+	
+    @OneToMany(mappedBy = "product",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Review> reviews=new ArrayList<>();
+	
+    @Column(name="num_ratings")
+    private int numRatings;
+    
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+	private Category category;
+    
+    @Column(name = "creadtedDate") 
+	private LocalDateTime createdAt;
+    
+    
+
+	public Product() {
+		super();
+	}
+
+	public Product(Long id, String title, String description, float price, int discountedPrice, int discountPersent,
+			int quantity, String brand, String imageUrl, List<Rating> ratings, List<Review> reviews, int numRatings,
+			Category category, LocalDateTime createdAt) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.price = price;
+		this.discountedPrice = discountedPrice;
+		this.discountPersent = discountPersent;
+		this.quantity = quantity;
+		this.brand = brand;
+		this.imageUrl = imageUrl;
+		this.ratings = ratings;
+		this.reviews = reviews;
+		this.numRatings = numRatings;
+		this.category = category;
+		this.createdAt = createdAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getDiscountedPrice() {
+		return discountedPrice;
+	}
+
+	public void setDiscountedPrice(int discountedPrice) {
+		this.discountedPrice = discountedPrice;
+	}
+
+	public int getDiscountPersent() {
+		return discountPersent;
+	}
+
+	public void setDiscountPersent(int discountPersent) {
+		this.discountPersent = discountPersent;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public List<Rating> getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(List<Rating> ratings) {
+		this.ratings = ratings;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	public int getNumRatings() {
+		return numRatings;
+	}
+
+	public void setNumRatings(int numRatings) {
+		this.numRatings = numRatings;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+    
+    
+	
 }
