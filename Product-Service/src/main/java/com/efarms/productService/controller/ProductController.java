6de5ab6cd2b4aa15cef0e8c2a6240583a27e9c90
Sridhar <sridhar.bssv@gmail.com>
2024-07-23 22:50:54 +0530

    product service implementation and category repository created

--- Product-Service/src/main/java/com/efarms/productService/controller/ProductController.java
+++ Product-Service/src/main/java/com/efarms/productService/controller/ProductController.java
@@ -1,5 +1,15 @@
 package com.efarms.productService.controller;
 
-public class ProductController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.efarms.productService.service.ProductService;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+	private ProductService productService;
+	public ProductController(ProductService productService) {
+		this.productService=productService;
+	}
 }
