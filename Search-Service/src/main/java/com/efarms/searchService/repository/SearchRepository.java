bc6fd2fd1d1ea54328fb91911899fa445c19c26c
Admin <Admin@DESKTOP-MIR7FFD>
2024-09-11 17:04:08 +0530

    Search Service updated

--- Search-Service/src/main/java/com/efarms/searchService/repository/SearchRepository.java
+++ Search-Service/src/main/java/com/efarms/searchService/repository/SearchRepository.java
@@ -1,5 +1,16 @@
 package com.efarms.searchService.repository;
 
-public interface SearchRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.efarms.searchService.entity.Search;
+
+public interface SearchRepository extends JpaRepository<Search, Long>{
+	
+	@Query("SELECT i FROM Item i WHERE LOWER(i.name) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+	           "OR LOWER(i.description) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+	    List<Search> searchByKeyword(@Param("keyword") String keyword);
 }
